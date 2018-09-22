196a0facc1b9b5eba86009f0384a7c09f45a893a
Jarek Kajko <jaruto7@gmail.com>
2018-09-22 23:33:45 +0200

    test2

--- components/App.js
+++ components/App.js
@@ -21,9 +21,9 @@ App = React.createClass({
     },
 
     getGif: function( searchingText, callback ) {
-        var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+        var url = GIPHY_LOADING_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
         var xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
+        xhr.open( 'GET', url );
         xhr.onload = function() {
             if( xhr.status === 200) {
                 var data = JSON.parse( xhr.responseText ).data;
